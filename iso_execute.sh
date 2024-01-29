b7456658b17f18d71d448e2e6929f82351dd09d2
vojkovic <brockv@tuta.io>
2024-01-29 13:10:52 +0800

    Format disks

--- iso_execute.sh
+++ iso_execute.sh
@@ -1,4 +1,9 @@
-# example command to see if it executes as expected
-fdisk -l
+#!/bin/bash
 
-lsblk
+# Partition and format disks & swap (3GiB)
+parted -s "/dev/vda" mklabel msdos
+parted -s "/dev/vda" mkpart primary 1MiB 29GiB
+parted -s "/dev/vda" mkpart primary linux-swap 29GiB 100%
+mkfs.ext4 "/dev/vda1"
+mkswap "/dev/vda2"
+swapon "/dev/vda2"
