9f8456ac186823ea93bfb55d1454ecb76aec6a5b
vojkovic <brockv@tuta.io>
2024-03-14 11:46:05 +0800

    Link age key to /root/infra/key.txt

--- files/root/4-Infra-Installation.sh
+++ files/root/4-Infra-Installation.sh
@@ -8,6 +8,9 @@ if [ ! -f /root/.config/sops/age/keys.txt ]; then
   exit 1
 fi
 
+# Link age key to /root/infra/key.txt
+ln -sf /root/.config/sops/age/keys.txt /root/infra/key.txt
+
 git clone https://github.com/vojkovic/infra.git --depth 1 /root/infra
 cd /root/infra
 
